909f8a1a769cde3df2464c61e4bfa90a9fa3cfec
Nayanin Alexander <nayanzin.alexander@gmail.com>
2019-02-01 13:08:21 +0200

    initial commit

---
+++ user-application/src/main/java/com/nayanzin/demohistrixuserapplication/DemoHistrixUserApplication.java
@@ -0,0 +1,14 @@
+package com.nayanzin.demohistrixuserapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHistrixUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHistrixUserApplication.class, args);
+    }
+
+}
+
---
+++ user-recomendations/src/test/java/com/nayanzin/demohistrixuserrecomendations/DemoHistrixUserRecomendationsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nayanzin.demohistrixuserrecomendations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoHistrixUserRecomendationsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
